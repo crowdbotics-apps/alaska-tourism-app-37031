e14ebfcb2f5aae7cad459b4aa1cca4a90b9d6a9e
Crowdbotics <team@crowdbotics.com>
2022-09-09 18:53:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "alaska_tourism_app_37031",
+  url: "https://alaska_tourism_app_37031.botics.co",
+  api: "https://alaska_tourism_app_37031.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
